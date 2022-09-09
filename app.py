b117053c4786ac8c95c9a887599639078fdcd157
Eddie Chapman <eddie.chapman4@gmail.com>
2022-09-09 18:00:23 -0500

    add `.html` to route decorater to correct flask-frozen urls

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact.html")
 def contact():
     return render_template("contact.html")
 
